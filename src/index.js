ae719c64f4aa4083ba2790067782b98d1f1f175a
Roman Liutikov <roman01la@romanliutikov.com>
2016-06-23 00:36:19 +0300

    remove @horizon/client

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { default as Horizon } from '@horizon/client/dist/horizon';
 export { default as connect } from './connect';
 export { default as HorizonProvider } from './provider';
 export { default as HorizonRoute } from './route';
