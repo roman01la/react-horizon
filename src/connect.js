2c8efb24750bd3a4cc49117a9902177c08a54fc7
Roman Liutikov <roman01la@romanliutikov.com>
2016-06-27 20:47:13 +0300

    impl basic global subscriptions management

--- src/connect.js
+++ src/connect.js
@@ -1,12 +1,19 @@
 import React, { Component, PropTypes } from 'react';
 import shallowequal from 'shallowequal';
+import hash from 'object-hash';
 
 export default function connect(ReactComponent, { subscriptions = {}, mutations = {} }) {
+
   return class extends Component {
+
     static displayName = `Horizon(${ReactComponent.displayName || ReactComponent.name})`
+
     static contextTypes = {
-      hz: PropTypes.func
+      hz: PropTypes.func,
+      subscribe: PropTypes.func,
+      unsubscribe: PropTypes.func,
     }
+
     constructor(props, context) {
 
       super(props, context);
@@ -19,38 +26,51 @@ export default function connect(ReactComponent, { subscriptions = {}, mutations 
       this._subscribe = this._subscribe.bind(this);
       this._unsubscribe = this._unsubscribe.bind(this);
       this._createMutations = this._createMutations.bind(this);
+      this._handleUpdate = this._handleUpdate.bind(this);
     }
     shouldComponentUpdate(nextProps, nextState) {
       return shallowequal(nextProps, this.props) === false ||
              shallowequal(nextState, this.state) === false;
     }
     componentWillReceiveProps(nextProps) {
-      this._unsubscribe(this._subscriptions);
       this._subscribe(this.context.hz, nextProps);
     }
     componentWillMount() {
+      this._unsubscribe();
       this._subscribe(this.context.hz, this.props);
       this._createMutations(this.context.hz);
     }
     componentWillUnmount() {
-      this._unsubscribe(this._subscriptions);
+      this._unsubscribe();
     }
     _subscribe(hz, props) {
       Object.keys(subscriptions)
         .forEach((qname) => {
+
           const q = subscriptions[qname];
-          const subscription = q(hz, props).watch().subscribe((data) => this.setState({ [qname]: data }));
-          this._subscriptions.push(subscription);
+          const qsub = q(hz, props);
+          const qhash = hash(qsub._query);
+
+          // local mapping from query hash to query name
+          this._subscriptions[qhash] = qname;
+
+          this.context.subscribe(qhash, qsub, this._handleUpdate);
         });
     }
-    _unsubscribe(subscriptions) {
-      subscriptions.forEach((q) => q.unsubscribe());
+    _unsubscribe() {
+      this.context.unsubscribe(this._handleUpdate, Object.keys(this._subscriptions));
     }
     _createMutations(hz) {
       Object.keys(mutations)
         .forEach((mname) => {
           this._mutations[mname] = mutations[mname](hz);
         });
+    }
+    _handleUpdate(hash, data) {
+      // get query name by its hash and assign received data
+      this.setState({
+        [this._subscriptions[hash]]: data
+      });
     }
     render() {
       return <ReactComponent {...this.props} {...this.state} {...this._mutations} />;
--- src/provider.js
+++ src/provider.js
@@ -5,15 +5,69 @@ class HorizonProvider extends Component {
     instance: PropTypes.func,
   }
   static childContextTypes = {
-    hz: PropTypes.func
+    hz: PropTypes.func,
+    subscribe: PropTypes.func,
+    unsubscribe: PropTypes.func,
   }
   getChildContext() {
-    return { hz: this.props.instance };
+    return {
+      hz: this.props.instance,
+      subscribe: this._handleSubscription,
+      unsubscribe: this._handleUnsubscription,
+    };
   }
   constructor(props, context) {
     super(props, context);
 
     this.props.instance.connect();
+
+    // subscriptions storage
+    this._subscriptions = {};
+
+    this._listeners = {};
+
+    // remote data storage
+    this.state = {};
+
+    this._handleSubscription = this._handleSubscription.bind(this);
+    this._updateListeners = this._updateListeners.bind(this);
+    this._handleUnsubscription = this._handleUnsubscription.bind(this);
+  }
+  _handleSubscription(qhash, query, fn) {
+
+    // register listener function
+    this._listeners[qhash] = this._listeners[qhash] || [];
+    this._listeners[qhash].push(fn);
+
+    // create subscription if it was not created before
+    if (this._subscriptions[qhash] === undefined) {
+
+      const sub = query.watch().subscribe((data) => {
+        const hash = qhash;
+        // update listeners when remote data updates
+        this.setState({ [hash]: data }, () => this._updateListeners(hash))
+      });
+
+      this._subscriptions[qhash] = sub;
+    }
+  }
+  _updateListeners(hash) {
+    this._listeners[hash].forEach((fn) => fn(hash, this.state[hash]));
+  }
+  _handleUnsubscription(fn, hashes) {
+    hashes.forEach((hash) => {
+      const listeners = this._listeners[hash];
+
+      // filter out listener function for current query hash
+      this._listeners[hash] = listeners.filter((lfn) => lfn !== fn);
+
+      // if there are no listeners to current query
+      // unsubscribe and delete subscription
+      if (this._listeners[hash].length === 0) {
+        this._subscriptions[hash].unsubscribe();
+        this._subscriptions[hash] = undefined;
+      }
+    });
   }
   render() {
     return React.Children.only(this.props.children);
